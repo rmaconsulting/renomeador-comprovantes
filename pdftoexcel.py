9c66a562a414bfdfd04c655133ba4a57202c48c7
Rodrigo Silva Ribeiro <rsribeiro86@gmail.com>
2025-01-17 08:58:53 -0300

    Remover todos os arquivos, exceto 'nome_do_arquivo.py'

--- pdftoexcel.py
+++
@@ -1,8 +0,0 @@
-import tabula
-import pandas as pd
-
-# Extrai as tabelas do PDF e as coloca em uma lista de DataFrames
-dfs = tabula.read_pdf("Descarga pagas - Outubro 2024.pdf", pages='all')
-
-# Escreve o primeiro DataFrame em um arquivo Excel
-dfs[0].to_excel("meu_excel.xlsx", index=False)
